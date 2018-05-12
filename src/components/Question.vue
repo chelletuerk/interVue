75241837d4341b0bf922c18e507f74ae6d87213a
chelle tuerk <chelle.tuerk@surveygizmo.com>
2018-05-12 09:44:12 -0600

    add input styling

--- src/components/Question.vue
+++ src/components/Question.vue
@@ -1,7 +1,8 @@
 <template>
   <div class="question">
     <h1>{{ msg }}</h1>
-    <input class="question-input" placeholder="Ask an intervue question"></input>
+    <input class="question-title-input" placeholder="Ask an intervue question"></input>
+    <input class="question-body-input" placeholder="Tell me about this question of yours"></input>
   </div>
 </template>
 
@@ -20,4 +21,20 @@ export default {
 h1 {
   font-weight: normal;
 }
+
+.question-title-input, .question-body-input {
+  width: 60%;
+  min-width: 200px;
+  height: 5vh;
+  margin: 1%;
+  border-radius: 4px;
+  border: solid #2c3e50 1px;
+}
+
+input::placeholder {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-size: 20px;
+  padding-left: 10px;
+}
+
 </style>
